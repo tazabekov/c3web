582a73fdf22cb5534ca7cb6e48a9a3f7b7fd519c
Gizzat Tazabekov <gizzat.tazabekov@c3iot.com>
2017-10-06 11:33:26 -0700

    better way of selecting child elements

--- components/counter-toggler/counter-toggler.js
+++ components/counter-toggler/counter-toggler.js
@@ -16,24 +16,19 @@ export class CounterToggler extends HTMLElement {
         super();
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
-        this._counterSlot = this.shadowRoot.querySelector('slot');
-
     }
     connectedCallback() {
         console.log("connectedCallback: CounterToggler");
 
-        this._upButton = this.shadowRoot.querySelector('#up');
-        this._downButton = this.shadowRoot.querySelector('#down');
-        this._counterSlot.addEventListener('slotchange', e => {
-            console.log("slotchange event on CounterToggler's slot");
-        });
+        this.shadowRoot.querySelector('#up')
+            .addEventListener('click', this._upClick.bind(this));
 
-        this._upButton.addEventListener('click', this._upClick.bind(this));
-        this._downButton.addEventListener('click', this._downClick.bind(this));
+        this.shadowRoot.querySelector('#down')
+            .addEventListener('click', this._downClick.bind(this));
     }
 
     _upClick(event) {
-        let bc = this.shadowRoot.querySelector( 'slot' ).assignedNodes()[1];
+        let bc = this.querySelector("binary-counter");
         let currentCounter = parseInt(bc.getAttribute("counter"));
         bc.setAttribute("counter", currentCounter+1);
     }
